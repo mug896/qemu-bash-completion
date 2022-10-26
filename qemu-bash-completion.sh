fc6f547e8becba957035d151d5edfb6fb2757cd5
Hyunho Cho <mug896@gmail.com>
2022-10-27 03:04:05 +0900

    update

--- qemu-bash-completion.sh
+++ qemu-bash-completion.sh
@@ -127,6 +127,8 @@ _qemu_global()
 
 _qemu_system() 
 {
+    # It is recommended that every completion function starts with _init_comp_wordbreaks,
+    # whether or not they change the COMP_WORDBREAKS variable afterward.
     _init_comp_wordbreaks
     [[ $COMP_WORDBREAKS != *","* ]] && COMP_WORDBREAKS+=","
     local IFS=$' \t\n' CUR CUR_O PREV PREV_O PREO CMD=$1 CMD2 WORDS COMP_LINE2
